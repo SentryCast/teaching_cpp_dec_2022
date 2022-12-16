20ce67b9b35909cac0d362a24742343ca00ffbd9
Vadim Kuznetsov <railfuner@gmail.com>
2022-12-16 23:22:11 +0500

    resetting changes

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -6,7 +6,7 @@ int main()
     std::ifstream fin("input.txt");
 
     int arr[6] = {1, 2, 3, 4, 5, 6};
-    // some change
+    
     int a, b, c;
     fin >> a >> b >> c;
     std::cout << arr[a] << std::endl;
