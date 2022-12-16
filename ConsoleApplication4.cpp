928824ace6dd0d6b424d07aa2534130573dae50f
Vadim Kuznetsov <railfuner@gmail.com>
2022-12-16 22:52:13 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ifstream fin("input.txt");
+
+    int arr[6] = { 1,2,3,4,5,6 };
+
+    int a, b, c;
+    fin >> a >> b >> c;
+    std::cout << arr[a] << std::endl;
+    std::cout << arr[b] << std::endl;
+    std::cout << arr[c] << std::endl;
+}
