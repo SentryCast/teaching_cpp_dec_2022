96d2b013a4fe5dacb2c1b402c4f530ebb161a1ef
vk <sentrycastle@gmail.com>
2022-12-16 23:12:34 +0500

    ff

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::ifstream fin("input.txt");
 
-    int arr[6] = { 1,2,3,4,5,6 };
-
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    // some change
     int a, b, c;
     fin >> a >> b >> c;
     std::cout << arr[a] << std::endl;
